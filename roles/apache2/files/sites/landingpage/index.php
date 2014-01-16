af5963e72621abb64cd16bf7ea93db2c3b5fb58e
Spenser Jones <spenser.e.jones@gmail.com>
2014-01-16 12:19:20 -0700

    Adding landingpage site to Apache and optimizing config

---
+++ roles/apache2/files/sites/landingpage/index.php
@@ -0,0 +1,46 @@
+<?php
+if (strpos($_SERVER['REMOTE_ADDR'], '10.0.0') !== 0) {
+  header('HTTP/1.0 404 Not Found');
+  echo file_get_contents('./404.html');
+  return;
+}
+?>
+<!DOCTYPE html>
+<html class="no-js">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Simple Simple Staging</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+
+    <link rel="stylesheet" href="http://landingpage.<?php echo $_SERVER['HTTP_HOST']; ?>/css/normalize.min.css">
+    <link rel="stylesheet" href="http://landingpage.<?php echo $_SERVER['HTTP_HOST']; ?>/css/main.css">
+
+    <script src="http://landingpage.<?php echo $_SERVER['HTTP_HOST']; ?>/js/vendor/modernizr-2.6.2.min.js"></script>
+  </head>
+  <body>
+    <h1>Hello Simple Simple!</h1>
+
+    <ul>
+<?php
+$dir = '/var/www/shared/';
+if (is_dir($dir)) {
+  if ($dh = opendir($dir)) {
+    while (($file = readdir($dh)) !== false) {
+      if (filetype($dir . $file) === 'dir' && $file !== '.' && $file !== '..') {
+        echo "      <li><a href='http://$file.{$_SERVER['HTTP_HOST']}/'>$file</a></li>\n";
+		    //echo "      <li><a href='http://{$_SERVER['HTTP_HOST']}/$file'>$file</a></li>\n";
+      }
+    }
+    closedir($dh);
+  }
+}
+?>
+    </ul>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="http://landingpage.<?php echo $_SERVER['HTTP_HOST']; ?>/js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+    <script src="http://landingpage.<?php echo $_SERVER['HTTP_HOST']; ?>/js/main.js"></script>
+  </body>
+</html>
